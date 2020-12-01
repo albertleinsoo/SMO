e2cefc9343e80c5eb15219b0fffd5b7c58630683
andoLeinsoo <leinsoo@mail.ru>
2020-12-01 21:36:09 +0300

    Добавлен класс логирования. Использован стандартный, java Logger

--- src/Global.java
+++ src/Global.java
@@ -1,5 +1,12 @@
-public class Global {
-    public static void main(String[] args) {
+import java.util.logging.Logger;
 
+public class Global {
+    private static final Logger logger = Logger.getLogger(Global.class.getName());
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        logger.info("Start");
+        logger.info("Start");
+        System.out.println(System.getProperty("java.util.logging.config.file"));
     }
 }
